69bcbf6d9fbb60fd0b6c40637c6f625f9158386f
Michael Preston <michael.n.preston@gmail.com>
2019-05-25 15:11:39 -0500

    renamed event handlers removed non-functioning handlers renamed BackdropComponent to Backdrop renamed cards to loaders, card to option FIX: render() called twice ADD: pass the path of file to the loader

--- src/apps/Letters/containers/ModalLoad.jsx
+++ src/apps/Letters/containers/ModalLoad.jsx
@@ -7,7 +7,10 @@ import PropTypes from "prop-types";
 import React from "react";
 import GoogleDrive from "../../../shared/media/drive.png";
 import HardDrive from "../../../shared/media/hdd.png";
-import { convertFile } from "../../../shared/utilities/converter";
+import { inject, observer } from 'mobx-react'
+import { observable, action } from 'mobx'
+
+// import { convertFile } from "../../../shared/utilities/converter";
 // src\shared\utilities\converter.js
 ///File Import Strategies:
 // import { DiskFileLoader } from '../modules/docxLoaders/DiskFileLoader.ts'
@@ -16,12 +19,6 @@ import { convertFile } from "../../../shared/utilities/converter";
 // import * as Loaders from '../modules/docxLoaders/Loaders.ts'
 // import { Loaders } from '../modules/docxLoaders/Loaders.ts'
 // import DiskFileLoader from "../utilities/docxLoaders_js/DiskFileLoader";
-import { inject, observer } from 'mobx-react'
-import { observable, action } from 'mobx'
-
-
-
-
 
 const styles = theme => ({
     root: {
@@ -70,18 +67,17 @@ const styles = theme => ({
 // @inject('store')
 @observer
 class ModalLoad extends React.Component {
+
     constructor(props) {
         super(props);
-
+        this.reader = new FileReader();
         this.state = {
             open: false,
-            description: "select a file from your computer to edit"
+            description: "Select a file from your computer to edit."
         };
     }
 
     @observable open = true
-
-
     @action handleClose = () => {
         console.log('close')
         this.open = false
@@ -94,76 +90,35 @@ class ModalLoad extends React.Component {
         // this.props.onUpdate(false);
     };
 
-    handleSelection = type => {
-        console.log(type);
-
-
-
-        //todo: Have the type-selected Loader retrive the file, regardless of its implementation
-        // let loader = Loaders.getLoader(type)
-        // let loader = new DiskFileLoader();
-        // if (!loader) throw Error("disk file loader not initialized!");
-
-        // //Good:
-        // loader
-        //     .load()
-        //     .then(result => {
-        //         // console.log('result:\n', result);
-        //         console.log("file loaded: ", loader.path);
-        //         convertFile(loader.path);
-        //         // console.log("[MP] converter.html: ", converter.html);
-        //         this.handleClose();
-        //     })
-        //     .catch(console.log);
-
-        // console.log("test"); //runs async
-    };
-
     handleFile = (e) => {
-        console.log('handleFiles', e)
+        if (!this.reader)
+            throw new Error("A FileReader must be defined before a file may be loaded!");
+
         const file = e.target.files[0]
-        console.log('File: ', file)
+        this.reader.readAsText(file);
 
-        const reader = new FileReader()
-
-        let html = ''
-
-        reader.addEventListener('load', (reader, i) => {
-            console.log('reader', reader.result)
-            // html += 'File starts with "' + reader.result.substr(0, 25) + '"'
-            // setTimeout(()=>{
-            //     console.log('finished', html)
-            // }, 5000)
-        })
-
-        reader.readAsText(file)
-
+        // console.log('handleFile event: ', e)
+        console.log('File metadata: ', file)
     }
 
     render() {
-        const { classes, store } = this.props;
-        // const {  } = lettersStore
+        console.log("RENDERING...");
 
-        console.log(this.open)
-
-        const cards = [
+        const { classes } = this.props;
+        const loaders = [
             {
                 name: "From Disk",
                 description: "Open a file from your computer's hard drive",
                 icon: HardDrive,
                 enabled: true,
-                handler: () => {
-                    this.handleSelection("disk");
-                }
+                handler: (e) => this.handleFile(e)
             },
             {
                 name: "Coming Soon",
                 description: "Open a file from your linked Google Drive folder",
                 icon: GoogleDrive,
                 enabled: false,
-                handler: () => {
-                    this.handleSelection("google");
-                }
+                handler: (e) => this.handleFile(e)
             },
             // {
             //     name: "Clipboard",
@@ -184,32 +139,53 @@ class ModalLoad extends React.Component {
                 onClose={this.handleClose}
                 onBackdropClick={this.handleClose}
                 disablePortal
-            // BackdropComponent={BackdropComponent}
+            // BackdropComponent={Backdrop}
             // fullScreen
             // container={this.props.container}
             // disablePortal
             // maxWidth={false}
             >
                 {/* <Grid container className={classes.demo} spacing={0} justify="space-evenly" alignItems="center"   > */}
-                {cards.map(card => {
+                {loaders.map(option => {
+
+                    const { name, enabled, handler } = option;
+                    const { grid } = classes;
+
                     return (
-                        <Grid key={card.name.toLocaleLowerCase()} item className={classes.grid} onClick={card.enabled ? card.handler : null} >
-                            <img src={card.icon} className={classes.icon} alt="cardimg" />
-                            <Button variant="contained" color="inherit" disabled={!card.enabled}>
-                                {card.name}
+                        <Grid
+                            key={name.toLocaleLowerCase()}
+                            item className={grid}
+                        // onClick={enabled ? handler : null}
+                        >
+                            <img
+                                src={option.icon}
+                                className={classes.icon}
+                                alt="cardimg" />
+                            <Button
+                                disabled={!enabled}
+                                variant="contained"
+                                color="inherit" >
+                                {name}
                             </Button>
-                            <input type="file" onChange={(e) => this.handleFile(e)} multiple></input>
+                            <input
+                                onChange={handler}
+                                type="file"
+                                multiple></input>
                         </Grid>
                     );
                 })}
                 {/* </Grid> */}
-                <DialogContentText align="center" className={classes.textbox}> {this.state.description} </DialogContentText>
+                <DialogContentText
+                    align="center"
+                    className={classes.textbox}>
+                    {this.state.description}
+                </DialogContentText>
             </Dialog>
         );
     }
 }
 
-const BackdropComponent = () =>
+const Backdrop = () =>
     <div style={{ height: 300, width: 300, background: 'red', zIndex: -1 }} />
 
 ModalLoad.propTypes = {
