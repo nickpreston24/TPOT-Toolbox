84b748d12811feb3d9002ec42d011c3b6b2bebc9
Michael Preston <michael.n.preston@gmail.com>
2019-06-11 22:40:03 -0500

    following firebase pattern: - Export firebase user management api - Export firebase Contexts - Export firebase HOCs

--- src/shared/firebase/index.js
+++ src/shared/firebase/index.js
@@ -1,9 +1,14 @@
-import * as auth from './auth';
-import * as db from './db';
-import * as firebase from './firebase';
+import Firebase from "./firebase";
+import FirebaseContext, { withFirebase } from './context';
 
-export {
-    auth,
-    db,
-    firebase,
-};
+export default Firebase;
+export { FirebaseContext, withFirebase };
+
+// import * as auth from './auth'; //Omitting for now as firebase.js covers most of the code. I can see we want some mobx involvement -MP
+// import * as db from './db';
+// import * as firebase from './firebase';
+// export {
+// auth,
+// db,
+// firebase,
+// };
