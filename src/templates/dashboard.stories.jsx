21c28234754290224435f4fe42b01dc718ba1a53
Braden Preston <bpfilmsinc@gmail.com>
2019-07-04 15:45:06 -0500

    Material UI updated to version 4.0

--- src/templates/dashboard.stories.jsx
+++ src/templates/dashboard.stories.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
 import { Dashboard } from '../apps/Toolbox/views/Dashboard'
-import { withKnobs, select } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 
-const variant = select (
-    'Variant', 
-    {
-        Default: 'default',
-        Empty: 'empty',
-        Loading: 'loading',
-        Error: 'error'
-    },
-    'default',
-    'gp10'
-)
-
 storiesOf('Toolbox/Dashboard', module)
-    .add('dynamic', () => <Dashboard {...{variant}} />)
+    .add('default', () => <Dashboard  />)
+    .add('loading', () => <Dashboard variant='loading' />)
+    .add('error', () => <Dashboard variant='error' />)
