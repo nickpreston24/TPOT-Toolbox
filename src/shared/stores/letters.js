1fca1b8b82a5ecf4ba2f2f0c915d8302cc205c85
Braden Preston <bpfilmsinc@gmail.com>
2019-08-31 15:17:40 -0500

    see if rootStore is not initializing

--- src/shared/stores/letters.js
+++ src/shared/stores/letters.js
@@ -30,7 +30,7 @@ class PublishData {
 export default class LettersStore {
     constructor(rootStore) {
         this.rootStore = rootStore
-        this.notify = rootStore ? rootStore.servicesStore.notify : () => { }
+        // this.notify = rootStore ? rootStore.servicesStore.notify : () => { }
     }
 
     @persist @observable clean = true
