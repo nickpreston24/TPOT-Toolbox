0032400774339dd3a68dd2fbe91563d53e9d1cf7
Braden Preston <bpfilmsinc@gmail.com>
2019-09-04 22:16:55 -0500

    remove duplicate import

--- src/apps/Scribe/index.jsx
+++ src/apps/Scribe/index.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { compose } from 'recompose'
 import { inject, observer } from 'mobx-react';
 import Editor from '../Editor/Editor';
-import { Route} from 'react-router-dom'
 import { Tab, Tabs, SvgIcon, Switch } from '@material-ui/core'
 import { PublishPage } from './views/PublishPage';
 import { Route, Redirect, Switch } from 'react-router-dom'
