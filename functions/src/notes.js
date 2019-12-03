ef862f5ddd9d59bb3b807ddfe5b05d2b21dfe808
mikepreston17 <michael.n.preston@gmail.com>
2019-12-02 22:56:57 -0600

    WIP - Added first cloud functions!

---
+++ functions/src/notes.js
@@ -0,0 +1,89 @@
+/** Sample from: https://medium.com/@alfianlosari/serverless-node-js-rest-api-with-google-cloud-function-firestore-d7b422f58511*/
+import { Router } from 'express';
+import admin from 'firebase-admin';
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
+
+const db = admin.firestore();
+const router = Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const noteSnapshot = await db.collection('notes').get();
+        const notes = [];
+        noteSnapshot.forEach((doc) => {
+            notes.push({
+                id: doc.id,
+                data: doc.data()
+            });
+        });
+        res.json(notes);
+    } catch(e) {
+        next(e);
+    }
+});
+
+router.get('/:id', async(req, res, next) => {
+    try {
+        const id = req.params.id;
+        if (!id) throw new Error('id is blank');
+        const note = await db.collection('notes').doc(id).get();
+        if (!note.exists) {
+            throw new Error('note does not exists');
+        }
+        res.json({
+            id: note.id,
+            data: note.data()
+        });
+    } catch(e) {
+        next(e);
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const text = req.body.text;
+        if (!text) throw new Error('Text is blank');
+        const data = { text };
+        const ref = await db.collection('notes').add(data);
+        res.json({
+            id: ref.id,
+            data
+        });
+    } catch(e) {
+        next(e);
+    }
+});
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const text = req.body.text;
+        if (!id) throw new Error('id is blank');
+        if (!text) throw new Error('Text is blank');
+        const data = { text };
+        res.json({
+            id,
+            data
+        });
+    } catch(e) {
+        next(e);
+    }
+});
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        if (!id) throw new Error('id is blank');
+        await db.collection('notes').doc(id).delete();
+        res.json({
+            id
+        });
+    } catch(e) {
+        next(e);
+    }
+});
+
+export default router;
--- functions/upload.spec.js
+++
@@ -1,9 +0,0 @@
-// Cloud storage SDK TEST
-// const admin = require('firebase-admin');
-// const serviceAccount = require('./serviceAccountKey.json.js')
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//     storageBucket: "tpot-toolbox"
-// })
-
-
