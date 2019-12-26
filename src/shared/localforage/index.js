7596af83e0b0a516c2e813053fd7dd80b9e03549
mikepreston17 <michael.n.preston@gmail.com>
2019-12-26 13:42:34 -0600

    Removed axios, videoforage and ytdl-core

--- src/shared/localforage/index.js
+++
@@ -1,9 +0,0 @@
-import localForage from 'localforage';
-import videoForage from './videoForage'
-
-export default class Forage {
-    constructor() {
-        this.root = this
-        this.videos = new videoForage(localForage)
-    }
-}
